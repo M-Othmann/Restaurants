20c88e627bf949a9668b2184e595b94da26eae6e
othman <othmanelrabiey@gmail.com>
2025-02-01 19:59:36 +0200

    Assigning user roles

--- Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
+++ Restaurants.Infrastructure/Seeders/RestaurantSeeder.cs
@@ -32,10 +32,18 @@ internal class RestaurantSeeder(RestaurantsDbContext _db) : IRestaurantSeeder
     {
         List<IdentityRole> roles =
             [
-                new (UserRoles.User),
-                new (UserRoles.Owner),
-                new (UserRoles.Admin),
-
+                new (UserRoles.User)
+                {
+                    NormalizedName = UserRoles.User.ToUpper()
+                },
+                new (UserRoles.Owner)
+                {
+                    NormalizedName = UserRoles.Owner.ToUpper()
+                },
+                new (UserRoles.Admin)
+                {
+                    NormalizedName = UserRoles.Admin.ToUpper()
+                },
             ];
         return roles;
     }
