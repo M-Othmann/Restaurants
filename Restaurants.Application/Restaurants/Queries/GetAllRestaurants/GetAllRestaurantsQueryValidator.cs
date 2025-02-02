cf19bfc57b320141b8618e31b1cdcfa8b8ee904d
othman <othmanelrabiey@gmail.com>
2025-02-02 18:53:06 +0200

    pagination request validation

---
+++ Restaurants.Application/Restaurants/Queries/GetAllRestaurants/GetAllRestaurantsQueryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace Restaurants.Application.Restaurants.Queries.GetAllRestaurants;
+
+public class GetAllRestaurantsQueryValidator : AbstractValidator<GetAllRestaurantsQuery>
+{
+
+    private int[] allowPageSizes = [5, 10, 15, 20];
+    public GetAllRestaurantsQueryValidator()
+    {
+        RuleFor(r => r.PageNumber)
+            .GreaterThanOrEqualTo(1);
+
+        RuleFor(r => r.PageSize)
+            .Must(value => allowPageSizes.Contains(value))
+            .WithMessage($"Page size must ve in [{string.Join(",", allowPageSizes)}]");
+
+
+    }
+}
